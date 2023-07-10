00e69cbce90e26bb475727d28119d0f885fa585c
masoud <mansory.php@gmail.com>
2023-07-10 07:37:32 +0330

    Edit Route

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('index');
 
-Route::get('dev', function () {
-    return 'dev';
-});
-
 Route::get('/post/{post:slug}', [\App\Http\Controllers\SiteController::class, 'show'])->name('post.show');
 Route::view('/form', 'form.form')->name('form');
 Route::post('form',[\App\Http\Controllers\SiteController::class, 'form'])->name('form.store');
