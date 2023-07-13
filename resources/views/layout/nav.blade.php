9b85c97e36b9fd10824f39c5cdcd801ec5660a37
masoud <mansory.php@gmail.com>
2023-07-13 23:21:24 +0330

    html edit

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark fixed-top">
     <div class="container py-2 mx-5">
         <a class="nav-link text-light" href="{{ route('index') }}">خانه</a>
-        <a class="nav-link text-light" href="{{ route('form') }}">لیست قیمت</a>
         <a class="nav-link text-light" href="{{ route('form') }}">فرم درخواست</a>
     </div>
 </nav>
